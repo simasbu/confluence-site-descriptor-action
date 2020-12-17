9f2c4c94a4ac2a79d7a33d417922b8d29abe805a
simasbu <simasbunev@gmail.com>
2020-12-17 11:51:51 +0100

    merge from master

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,17 +1,17 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import * as process from 'process';
+import * as cp from 'child_process';
+import * as path from 'path';
 
 test('success flow', () => {
-  process.env['INPUT_LOCALDIRECTORY'] = 'src'
-  process.env['INPUT_OUTPUTDIRECTORY'] = 'src/newage/somepage'
-  process.env['INPUT_HOMEPAGETITLE'] = 'My title'
-  process.env['INPUT_PARENTPAGETITLE'] = 'Home page title'
-  process.env['INPUT_SPACEKEY'] = 'My space'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  process.env['INPUT_LOCALDIRECTORY'] = 'src';
+  process.env['INPUT_OUTPUTDIRECTORY'] = 'src/newage/somepage';
+  process.env['INPUT_HOMEPAGETITLE'] = 'My title';
+  process.env['INPUT_PARENTPAGETITLE'] = 'Home page title';
+  process.env['INPUT_SPACEKEY'] = 'My space';
+  const np = process.execPath;
+  const ip = path.join(__dirname, '..', 'lib', 'main.js');
   const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+    env: process.env,
+  };
+  console.log(cp.execFileSync(np, [ip], options).toString());
+});
